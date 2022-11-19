194d8805d9df4af712ce4157dba66db4c2d723bf
AngoNex <71973953+AngoNex@users.noreply.github.com>
2022-11-19 16:20:19 +0300

    структура

--- lua/autorun/client/spels_cl.lua
+++ lua/autorun/client/spels_cl.lua
@@ -1,21 +1,23 @@
 AddCSLuaFile()
 
 local SpelList = {
-    ["Blink"] = { delay = 5,
-    cooldown = 0,
-    timestart = 0,
-    prefunction = function( ply )
+    ["Blink"] = {
+        delay = 5,
+        cooldown = 0,
+        timestart = 0,
+        prefunction = function( ply )
 
-    end,
-    postfunction = function( ply )
+        end,
+        postfunction = function( ply )
 
-    end,
-},
+        end,
+    },
 
 }
 
 hook.Add( "InitPostEntity", "Ready", function()
-	ply.TablePhaseSpels = ply.TablePhaseSpels or {}
+    local ply = LocalPlayer()
+	ply.TableSpels = ply.TablePhaseSpeTableSpelsls or {}
 end )
 
 net.Receive("UpdatePlayerSpel", function()
@@ -32,6 +34,31 @@ end)
 
 hook.Add( "UpdatePlayerSpel", "UpdatePlayerSpel", function( ply, owner, type, spel )
 	if IsValid( ply ) then
+        if type == "init" then
+            -- добавить обилку в массив игрока
+            -- в том числе и создать объект spel на стороне клиента
+        end
 
+        if type == "cooldown" then
+            -- включить отсчёт кд ( ну и худ соответственно тоже )
+        end
+
+        if type == "pre" then
+            -- включить пре функцию ( например отрисовка траектории )
+            -- выключение пре функции должно быть либо при начале пост функции либо при отмене
+        end
+
+        if type == "cancel" then
+            -- отмена пре функции если она была активирована
+        end
+
+        if type == "post" then
+            -- включить отсчёт кд ( ну и худ соответственно тоже )
+            -- включить пост функцию ( само действие )
+        end
+
+        if type == "remove" then
+            -- удаляет обилку из массива игрока
+        end
     end
 end )
--- lua/autorun/server/spels_sv.lua
+++ lua/autorun/server/spels_sv.lua
@@ -11,13 +11,21 @@ hook.Add( "PlayerButtonDown", "Spels_Base", function( ply, key )
             local spels = ply.TableSpels
 
             for name, spel in pairs( spels ) do
-                if key == spel.key and spel.prefunction != false then
-                    spel.prefunction( ply )
+                if key == spel.key and (spel.prefunction != false or spel.postfunction != false) then
 
                     local nettable = {}
                     nettable.owner = ply
                     nettable.spel = name
-                    nettable.type = "pre"
+
+                    if spel.prefunction != false then
+                        nettable.type = "pre"
+                        spel.prefunction( ply )
+                    elseif spel.postfunction != false then
+                        nettable.type = "post"
+                        spel.postfunction( ply )
+                    else
+                        return
+                    end
 
                     if spel.sendtoclient then
                         nettable.sendtoclient = spel.sendtoclient
@@ -47,7 +55,7 @@ hook.Add( "PlayerButtonUp", "Spels_Base", function( ply, key )
             local spels = ply.TableSpels
 
             for name, spel in pairs( spels ) do
-                if key == spel.key and spel.postfunction != false then
+                if key == spel.key and spel.postfunction != false and spel.prefunction != false then
                     isfunction( spel.postfunction( ply ) )
 
                     local nettable = {}
